b083e7f7ded14c49d106563d39147d69c6efb49d
alentoghostflame <alexanderschoenhofen@gmail.com>
2019-02-22 15:08:54 -0600

    Added comments.

--- Globals.py
+++ Globals.py
@@ -1,11 +1,16 @@
 '''
-
 The file that contains all the storage locations that will be used in between threads.
-
 '''
 
 
 class GlobalVariables:
+    '''
+    Used for easy communication between threads throughout the soundboard.
+    input: See InputClass.
+    online: See OnlineClass.
+    file: See FileClass.
+    misc: See MiscClass.
+    '''
     def __init__(self):
         self.input = InputClass()
         self.online = OnlineClass()
@@ -14,6 +19,17 @@ class GlobalVariables:
 
 
 class InputClass:
+    '''
+    Holds information about input.
+    key: Integer representing the last key pressed, written to by the key_detector, read by the file_controller
+    and audio_controller.
+    page: Integer representing what page/folder the user wants to play sounds from, written to and read from by
+    the file_controller
+    write_ready: Holds a boolean representing what last wrote or read from key. If write_ready is True, that means that
+    the file_controller or audio_controller has processed key, and key_detector cannot write a new key. If write_ready
+    is False, that means key_detector can write a new value to key, and file_controller and audio_controller cannot read
+    key.
+    '''
     def __init__(self):
         self.key = 666
         self.page = 1
@@ -21,6 +37,14 @@ class InputClass:
 
 
 class OnlineClass:
+    '''
+    Holds information concerning the status of the threads.
+    main: Boolean representing if main is online or not.
+    key_detector: Boolean representing if the key_detector is online or not.
+    audio_controller: Boolean representing if the audio_controller is online or not.
+    file_controller: Boolean representing if the file_controller is online or not.
+    display_controller: Boolean representing if the display_controller is online or not.
+    '''
     def __init__(self):
         self.main = False
         self.key_detector = False
@@ -30,6 +54,14 @@ class OnlineClass:
 
 
 class FileClass:
+    '''
+    Holds information concerning the file system.
+    folder_index: List of strings representing names of folders
+    folder_path_index: List of strings representing paths of folders.
+    total_folders: Integer representing the number of folders.
+    file_index: List of strings representing names of files.
+    file_path_index: List of strings representing paths of files.
+    '''
     def __init__(self):
         self.folder_index = []
         self.folder_path_index = []
@@ -39,6 +71,12 @@ class FileClass:
 
 
 class MiscClass:
+    '''
+    Miscellaneous variables that don't quite fit in the above categories, or need a category of their own.
+    quit: Boolean that represents if the program should quit or not. Written to by main, read by all threads.
+    os_detected: Integer that represents what operating system the host is running. See Dictionaries.OS_DICT for values,
+    event_blocker: Boolean the key_detector uses to delay main until the key_detector is online.
+    '''
     def __init__(self):
         self.quit = False
         self.os_detected = 0
