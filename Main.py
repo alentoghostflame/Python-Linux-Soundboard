55d3371aaa2669510312189911f7d468171a9922
alentoghostflame <alexanderschoenhofen@gmail.com>
2019-03-01 16:27:17 -0600

    Didn't change too much, but runs the GUI I guess.

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 
 The main start file for Python Voice Chat Soundboard.
@@ -7,39 +8,43 @@ Main.py starts up the threads and tells them to shut down when need be. This is 
 from time import sleep
 import sys
 from Globals import global_variables
+from Config import global_config
 from Logger import log, INFO, WARNING, ERROR
-from KeyDetectors import StartKeyDetection
+import InputController
 import AudioController
-import FileExplorer
+import FileController
 import DisplayController
 from Dictionaries import OS_DICT
 
 
 def main():
     '''
-    Initialized and runs all the various threads, then sleeps until a KeyboardInturrupt occurs.
+    Initialized and runs all the various threads, then sleeps until a KeyboardInterrupt occurs.
     '''
-    global_variables.misc.os_detected = DetectOS()
-    global_variables.input.key = 5
+    global_variables.misc.os_detected = detect_os()
+
     ''' If Windows is detected, enable ANSI escape stuff '''
-    if global_variables.misc.os_detected is 3:
+    '''if global_variables.misc.os_detected is 3:
         import ctypes
         kernel32 = ctypes.windll.kernel32
-        kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
+        kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)'''
 
-    StartKeyDetection()
+    InputController.start_input_controller()
     while global_variables.misc.event_blocker is True:
         sleep(0.5)
-    AudioController.start_audio_controller()
-    FileExplorer.init()
-    FileExplorer.StartFileController()
-    DisplayController.StartDisplayController()
+
+    if global_config.use_gui is False:
+        DisplayController.display_terminal_output()
+    else:
+        DisplayController.run_gui()
+
     global_variables.online.main = True
+
     while True:
         sleep(1)
 
 
-def DetectOS():
+def detect_os():
     return OS_DICT[sys.platform]
 
 
@@ -47,10 +52,16 @@ try:
     '''
     The purpose of this try-except is to catch Keyboard Inturrupts and properly close down the threads.
     '''
+    log(INFO, "main", "Beginning program execution.")
+    FileController.refresh_files()
+    AudioController.start_audio_setup()
     main()
 except KeyboardInterrupt:
     global_variables.misc.quit = True
     while global_variables.online.key_detector is True or global_variables.online.display_controller is True or \
-            global_variables.online.audio_controller is True or global_variables.online.file_controller is True:
+            global_variables.online.audio_controller is True or global_variables.online.file_controller is True or \
+            global_variables.online.input_controller is True:
         sleep(0.5)
-    print("All threads have quit, quitting!")
+
+    AudioController.end_audio_setup()
+    log(INFO, "main", "All threads have quit in some manner, quitting!")
