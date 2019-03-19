ff4c1e64fcc8ca259c4209cefa478a58d3e5b39f
alentoghostflame <alexanderschoenhofen@gmail.com>
2019-03-19 11:29:09 -0500

    Corrected existing comments and added new information.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
 
-The main start file for Python Voice Chat Soundboard.
-Main.py starts up the program and tells the various parts of it to shutdown when needed. The user should run this file.
+The main start file for Python Voice Chat Soundboard. The user should run this file.
+Main.py starts up the program, runs the frontend, and tells the various parts of it to shutdown when needed.
 
 """
 from time import sleep
@@ -15,8 +15,12 @@ import display_controller
 
 def main():
     """
-    Go into a display loop until the display is canceled for whatever reason.
-    TKinter demands to have the window be run in the main thread, unfortunately.
+    Report main as online.
+    Depending on the config setting, either run the terminal-based frontend or the TKinter based frontend.
+    Both of these frontend choices should be a loop of some kind. The try-catch below currently relies on that.
+
+    Reason for having main control the graphical frontend: TKinter demands that the window be run in the main thread,
+    and not a threading thread. To keep consistency and reduce complexity, run both options in the main thread.
     """
     global_variables.online.main = True
     if global_config.main.use_gui is False:
@@ -27,9 +31,17 @@ def main():
 
 try:
     """
-    Have the file_controller initially read everything, the audio_controller startup the null-sink and loopback, the 
-    input_controller get inputs (if needed), and finally get the main thread in the display loop.
-    Once the window is closed, main will tell all the threads to stop 
+    Create the PythonSoundboardOutput null-sink and (if specified in the config) Loopback.
+    Start up the input_controller thread.
+    Run the frontend.
+    
+    Once the frontend has exited, give the signal for all existing threads to terminate themselves, unload the
+    PythonSoundboardOutput null-sink and (if specified in the config) Loopback.
+    Then wait until all child threads to report offline before exiting.
+    
+    If a KeyboardInterrupt were to happen, give the signal for all existing threads to terminate themselves, unload the
+    PythonSoundboardOutput null-sink and (if specified in the config) Loopback.
+    Then wait until all child threads to report offline before exiting.
     """
     log(INFO, "main", "Beginning program execution.")
     # global_variables.misc.os_detected = detect_os()
@@ -38,14 +50,14 @@ try:
     main()
     global_variables.misc.quit = True
     audio_controller.end_audio_setup()
-    while global_variables.online.key_detector is True or global_variables.online.input_controller is True:
+    while global_variables.online.key_detector is True:
         sleep(0.5)
     log(INFO, "main", "All threads have quit in some manner, quitting!")
 
 except KeyboardInterrupt:
-    # If a keyboard interrupt is done for some reason, kill threads, null-sink, and loopback.
+    # If a keyboard interrupt is done for some reason, kill the threads, null-sink, and loopback.
     global_variables.misc.quit = True
     audio_controller.end_audio_setup()
-    while global_variables.online.key_detector is True or global_variables.online.input_controller is True:
+    while global_variables.online.key_detector is True:
         sleep(0.5)
     log(INFO, "main", "All threads have quit in some manner, quitting!")
